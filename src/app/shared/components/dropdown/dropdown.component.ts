6be67695035e1689715ccef017aeba5639a9e1a2
hisham-mhammed-afifi <hisham.mohmmed@gmail.com>
2023-12-09 00:08:54 +0200

    adjusted the dropdown component

--- src/app/shared/components/dropdown/dropdown.component.ts
+++ src/app/shared/components/dropdown/dropdown.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+export interface MenuItem {
+  label: string;
+  value: number;
+}
 
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
-  styleUrls: ['./dropdown.component.scss']
+  styleUrls: ['./dropdown.component.scss'],
 })
 export class DropdownComponent implements OnInit {
+  showList = false;
+  selectedItem: MenuItem = <MenuItem>{};
 
-  constructor() { }
+  @Input() label = '';
+  @Input() items: MenuItem[] = [];
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.selectedItem = this.items[0];
   }
-
 }
