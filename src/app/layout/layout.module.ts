70e56d87d06a63bab384416be4ab0605946fea2d
hisham-mhammed-afifi <hisham.mohmmed@gmail.com>
2023-12-09 02:44:36 +0200

    finalized the top bar component with adjustement for some styles related

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,17 +5,10 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { UsersTableComponent } from './components/users-table/users-table.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { LayoutComponent } from './layout.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    UsersTableComponent,
-    TopBarComponent,
-    LayoutComponent
-  ],
-  imports: [
-    CommonModule,
-    LayoutRoutingModule
-  ]
+  declarations: [UsersTableComponent, TopBarComponent, LayoutComponent],
+  imports: [CommonModule, LayoutRoutingModule, SharedModule],
 })
-export class LayoutModule { }
+export class LayoutModule {}
